dc3ce7d01d46b9c44ddb0111f75fcb86c7c6a96d
dcode-fullstack <dcode.fullstack@gmail.com>
2022-04-14 01:23:27 +0200

    project finished

--- app.js
+++ app.js
@@ -7,31 +7,6 @@ for (let i = 0; i < list.length; i++) {
     list[i].addEventListener("click", function () {
         removeSelect();
         list[i].classList.add('active');
-        for (let x = 0; x < content.length; x++) {
-
-            function send() {
-                const active = document.querySelector('.active');
-
-                if (active) {
-                    removeContent();
-                    console.log(list[i].textContent);
-                    content[1].classList.add('show');
-                    const selection = document.querySelector('#selection');
-                    const text = document.createTextNode(`You selected ${active.textContent} out of 5`);
-                    selection.appendChild(text);
-
-                } else {
-                    alert('You need to choose a Rate to Submit');
-                }
-
-            }
-
-            function removeContent() {
-                content[0].classList.remove('show');
-            }
-        }
-
-        submit.addEventListener('click', send);
     });
 
     function removeSelect() {
@@ -39,7 +14,28 @@ for (let i = 0; i < list.length; i++) {
     };
 };
 
+for (let x = 0; x < content.length; x++) {
 
+    function send() {
+        const active = document.querySelector('.active');
 
+        if (active) {
+            removeContent();
+            console.log(active.textContent);
+            content[1].classList.add('show');
+            const selection = document.querySelector('#selection');
+            const text = document.createTextNode(`You selected ${active.textContent} out of 5`);
+            selection.appendChild(text);
 
+        } else {
+            console.log('hello');
+            alert('You must enter a Rate to continue');
+        }
+    }
 
+    function removeContent() {
+        content[0].classList.remove('show');
+    }
+}
+
+submit.addEventListener('click', send);
